6988a2e0d1a8a569b73c29aa1ae4c50bffef4f5a
Jackson Hayward <jchayward1@gmail.com>
2022-10-24 14:24:54 -0700

    Addded comments and updated formatting

--- script.js
+++ script.js
@@ -4,54 +4,43 @@ solve.addEventListener('click', () => {
 	output.textContent = "hello there";
 })
 
-console.log("hello world!");
 console.log(ConcPM25(1));
 
 
 
 
-// functions from airnow site
 
-function ConcPM25(a) {
-	if (a>=0 && a<=50) {
-		ConcCalc=InvLinear(50,0,12,0,a);
-	} else if (a>50 && a<=100) {
-		ConcCalc=InvLinear(100,51,35.4,12.1,a);
-	} else if (a>100 && a<=150) {
-		ConcCalc=InvLinear(150,101,55.4,35.5,a);
-	} else if (a>150 && a<=200) {
-		ConcCalc=InvLinear(200,151,150.4,55.5,a);
-	} else if (a>200 && a<=300) {
-		ConcCalc=InvLinear(300,201,250.4,150.5,a);
-	} else if (a>300 && a<=400) {
-		ConcCalc=InvLinear(400,301,350.4,250.5,a);
-	} else if (a>400 && a<=500) {
-		ConcCalc=InvLinear(500,401,500.4,350.5,a);
+/* This function determines the value of the passed air quality and calls InvLinear with constant parameters and 
+the passed aqi value. Parameter values are sourced from airnow.gov */
+
+function findConcentration(aqi) {
+	let concentration;
+	if (aqi>=0 && aqi<=50) {
+		concentration=InvLinear(50,0,12,0,aqi);
+	} else if (aqi>50 && aqi<=100) {
+		concentration = InvLinear(100,51,35.4,12.1,aqi);
+	} else if (aqi>100 && aqi<=150) {
+		concentration = InvLinear(150,101,55.4,35.5,aqi);
+	} else if (aqi>150 && aqi<=200) {
+		concentration=InvLinear(200,151,150.4,55.5,aqi);
+	} else if (aqi>200 && aqi<=300) {
+		concentration=InvLinear(300,201,250.4,150.5,aqi);
+	} else if (aqi>300 && aqi<=400) {
+		concentration=InvLinear(400,301,350.4,250.5,aqi);
+	} else if (aqi>400 && aqi<=500) {
+		concentration=InvLinear(500,401,500.4,350.5,aqi);
 	} else {
-		ConcCalc = "Error: Provided air quality out of range. Please enter a value between 0 and 500";
+		concentration = "Error: Provided air quality out of range. Please enter a value between 0 and 500";
 	}
-	return ConcCalc;
+	return concentration;
 }
 
+/* Calculate estimatedd PM2.5 concentration from a series of constant parameters and a provided air quality value. This 
+function returns the concentration in units of micrograms per meter squared.  */
 
-function InvLinear(AQIhigh, AQIlow, Conchigh, Conclow, a) {
-	let c = ((a - AQIlow) / (AQIhigh - AQIlow)) * (Conchigh - Conclow) + Conclow;
-	return c;
+function InvLinear(AQIhigh, AQIlow, Conchigh, Conclow, aqi) {
+	let conc = ((aqi - AQIlow) / (AQIhigh - AQIlow)) * (Conchigh - Conclow) + Conclow;
+	return conc;
 }
 
-function AQICalc(form1) {
-	var b;
-	var c;
-
-	if (document.form1.pollutant.selectedIndex == '1') {
-
-		b = Math.round(document.form1.inputbox.value);
-
-		if (b < 0 || b>500) {		
-			b = "Error: Provided air quality out of range. Please enter a value between 0 and 500";
-		} else {
-			c = Math.floor(10*ConcPM25(b))/10;
-		}  		
-	}
-}
 
